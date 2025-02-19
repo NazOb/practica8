d602cced6360e99c85d0aa550115636ae6fc1af2
Nazario <nazobx@gmail.com>
2025-02-18 19:19:09 -0700

    commit

--- app/src/main/java/mx/edu/itesca/practica8/MainActivity.kt
+++ app/src/main/java/mx/edu/itesca/practica8/MainActivity.kt
@@ -1,6 +1,8 @@
 package mx.edu.itesca.practica8
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,13 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val btn:Button= findViewById(R.id.btn_in)
+
+        btn.setOnClickListener {
+            var intent : Intent = Intent(this,catalogo::class.java)
+            startActivity(intent)
         }
     }
 }
--- app/src/main/java/mx/edu/itesca/practica8/catalogo.kt
+++ app/src/main/java/mx/edu/itesca/practica8/catalogo.kt
@@ -1,6 +1,7 @@
 package mx.edu.itesca.practica8
 
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,6 +9,8 @@ import android.view.ViewGroup
 import android.widget.BaseAdapter
 import android.widget.GridLayout
 import android.widget.GridView
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.helper.widget.Grid
@@ -46,8 +49,11 @@ class catalogo : AppCompatActivity() {
     fun loadMovies(){
         movies.add(Movies("Big Hero 6",R.drawable.bighero6,R.drawable.headerbighero6,"When a devastating event befalls the city of San Fransokyo and catapults Hiro into the midst of danger, he turns to Baymax and his close friends adrenaline junkie Go Go Tomago, neatnik Wasabi, chemistry whiz Honey Lemon and fanboy Fred. Determined to uncover the mystery, Hiro transforms his friends into a band of high-tech heroes called \"Big Hero 6.\""))
         movies.add(Movies("Leap year",R.drawable.leapyear,R.drawable.leapyearheader,"A woman who has an elaborate scheme to propose to her boyfriend on Leap Day, an Irish tradition which occurs every time the date February 29 rolls around, faces a major setback when bad weather threatens to derail her planned trip to Dublin. With the help of an innkeeper, however, her cross-country odyssey just might result in her getting engaged."))
-        series.add(Movies("",R.drawable.drhouse,R.drawable.drhouse,"The series follows the life of anti-social, pain killer addict, witty and arrogant medical doctor Gregory House (Hugh Laurie) with only half a muscle in his right leg. He and his team of medical doctors try to cure complex and rare diseases from very ill ordinary people in the United States of America."))
-        series.add(Movies("",R.drawable.smallville,R.drawable.smallvilleheader,"The numerous miraculous rescues by the local wonder boy Clark have aroused suspicions amongst colonials of Smallville. Interestingly, the boy has managed to downplay his acts of various heroic egresses in the past. They say he's either too fast or has a penchant for finding trouble. He was found by Martha and Jonathan Kent on the day of the Meteor Shower, and subsequently adopted. Clark's friend Lex Luthor, the only heir of Luthorcorp, has been secretly investigating grounds for Clark's outlandish valor. However, on the face of it, Clark just seems a normal boy who's slightly more secretive than usual."))
+        movies.add(Movies("Men in Black",R.drawable.mib,R.drawable.mibheader,"Based off of the comic book. Unbeknownst to other people, there is a private agency code named MiB. This agency is some kind of extra terrestrial surveillance corporation. Then, one of the agency's finest men only going by the name \"K\" (Tommy Lee Jones) , is recruiting for a new addition to the agency. He has chosen James Edwards (Will Smith) of the N.Y.P.D. Then, one day, a flying saucer crashes into Earth. This was an alien a part of the \"Bug\" race. He takes the body of a farmer (Vincent D'Onofrio) and heads to New York. He is searching for a super energy source called \"The Galaxy\". Now, Agents J and K must stop the bug before it can escape with the galaxy."))
+
+        series.add(Movies("Dr. House",R.drawable.drhouse,R.drawable.drhouse,"The series follows the life of anti-social, pain killer addict, witty and arrogant medical doctor Gregory House (Hugh Laurie) with only half a muscle in his right leg. He and his team of medical doctors try to cure complex and rare diseases from very ill ordinary people in the United States of America."))
+        series.add(Movies("Smallville",R.drawable.smallville,R.drawable.smallvilleheader,"The numerous miraculous rescues by the local wonder boy Clark have aroused suspicions amongst colonials of Smallville. Interestingly, the boy has managed to downplay his acts of various heroic egresses in the past. They say he's either too fast or has a penchant for finding trouble. He was found by Martha and Jonathan Kent on the day of the Meteor Shower, and subsequently adopted. Clark's friend Lex Luthor, the only heir of Luthorcorp, has been secretly investigating grounds for Clark's outlandish valor. However, on the face of it, Clark just seems a normal boy who's slightly more secretive than usual."))
+        series.add(Movies("Dr. Who",R.drawable.drwho,R.drawable.drwhoheader,"Traveling across time and space, the immortal time-lord known as 'The Doctor' travels across the universe with his many companions and his loyal shape-shifting space-ship: The TARDIS. The Doctor faces many threats across many generations: from The Daleks, The Cybermen and his time-lord adversary The Master to the sinister Davros, creator of The Daleks."))
 
     }
 
@@ -76,7 +82,21 @@ class catalogo : AppCompatActivity() {
             var movie = movies[position]
             var inflator = context!!.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
             var vista = inflator.inflate(R.layout.movies,null)
-//            flat
+            var image:ImageView = vista.findViewById(R.id.iv_movie_cell)
+            var title:TextView = vista.findViewById(R.id.tv_movie_title_cell)
+
+            image.setImageResource(movie.image)
+            title.setText(movie.title)
+
+            image.setOnClickListener {
+                val intent = Intent(context,movie_detaill::class.java)
+                intent.putExtra("title",movie.title)
+                intent.putExtra("image",movie.image)
+                intent.putExtra("header",movie.header)
+                intent.putExtra("sinopsis",movie.sinopsis)
+                context!!.startActivity(intent)
+            }
+            return vista
         }
     }
 }
